082acf691152b285f464a78cf97301c34307b036
Martin PAUCOT <contact@martin-paucot.fr>
2024-10-10 00:18:23 +0200

    fix(vite): optimize deps

--- src/plugins/vite.ts
+++ src/plugins/vite.ts
@@ -1,10 +1,30 @@
-import Components from 'unplugin-vue-components/vite'
-import { PrimeVueResolver } from '@primevue/auto-import-resolver'
-
+// TODO: Find a way to either optimize everything from primevue or have an entrypoint that do it
 export default function cockpit() {
-  return [
-    Components({
-      resolvers: [PrimeVueResolver()],
+  return {
+    name: 'cockpit',
+    config: () => ({
+      optimizeDeps: {
+        include: [
+          'qs',
+          'primevue/toast',
+          'primevue/usetoast',
+          'primevue/useconfirm',
+          'primevue/button',
+          'primevue/datatable',
+          'primevue/inputtext',
+          'primevue/breadcrumb',
+          'primevue/paginator',
+          'primevue/select',
+          'primevue/multiselect',
+          'primevue/menu',
+          'primevue/toggleswitch',
+          'primevue/togglebutton',
+          'primevue/badge',
+          'primevue/password',
+          'primevue/confirmdialog',
+          'primevue/confirmpopup',
+        ],
+      },
     }),
-  ]
+  }
 }
